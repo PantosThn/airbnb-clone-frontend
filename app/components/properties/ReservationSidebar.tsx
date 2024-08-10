9219d2a800482066dbfb074431671f8223ee462e
Thanos Pantos <pantos.thn@gmail.com>
2024-08-11 00:14:35 +0300

    added reservation bar

--- app/components/properties/ReservationSidebar.tsx
+++ app/components/properties/ReservationSidebar.tsx
@@ -1,6 +1,10 @@
 const ReservationSidebar = () => {
     return (
-        <p>adf</p>
+        <aside className="mt-4 p-6 col-span-2 rounded-xl border border-gray-300 shadow-xl">
+            <h2 className="mb-5 text-2xl">$200 per night</h2>
+
+            <div className="mb-6 p-3 border border-gray-400"></div>
+        </aside>
     )
 }
 
